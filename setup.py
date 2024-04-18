484d56aa7dc068771017998d64ac8a2add6f6803
Gregory <57194668+Greg50100@users.noreply.github.com>
2024-04-18 09:55:06 +0200

    create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='convertions',
+    version='0.1',
+    packages=find_packages(),
+    description='A package for various conversions',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/Greg50100/convertions.git',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
